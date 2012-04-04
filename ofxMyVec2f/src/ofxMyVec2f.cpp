d13199bf86786221c44843d3894498774c25d3ad
Kimon Satan <simon@simonkatan.co.uk>
2012-04-04 12:21:21 +0200

    forgotten files

---
+++ ofxMyVec2f/src/ofxMyVec2f.cpp
@@ -0,0 +1,80 @@
+#include "ofxMyVec2f.h"
+
+ofxMyVec2f::ofxMyVec2f()
+{
+    //ctor
+}
+
+int ofxMyVec2f::pointPolyTest(vector <ofxVec2f>* polygon){
+int counter = 0;
+  int i;
+  int N = polygon->size();
+  float xinters;
+  ofPoint p1,p2;
+
+  p1 = polygon->at(0);
+  for (i=1;i<=N;i++) {
+
+    p2 = polygon->at(i % N);
+    if (y > min(p1.y,p2.y)) {
+      if (y <= max(p1.y,p2.y)) {
+        if (x <= max(p1.x,p2.x)) {
+          if (p1.y != p2.y) {
+            xinters = (y-p1.y)*(p2.x-p1.x)/(p2.y-p1.y)+p1.x;
+
+            if (p1.x == p2.x || x <= xinters)
+              counter++;
+          }
+        }
+      }
+    }
+    p1 = p2;
+  }
+
+  if (counter % 2 == 0)
+    return(0);
+  else
+    return(1);
+
+}
+
+int ofxMyVec2f::pointPolyTest(vector <ofPoint>* polygon){
+
+int counter = 0;
+  int i;
+  int N = polygon->size();
+  float xinters;
+  ofPoint p1,p2;
+
+  p1 = polygon->at(0);
+  for (i=1;i<=N;i++) {
+
+    p2 = polygon->at(i % N);
+    if (y > min(p1.y,p2.y)) {
+      if (y <= max(p1.y,p2.y)) {
+        if (x <= max(p1.x,p2.x)) {
+          if (p1.y != p2.y) {
+            xinters = (y-p1.y)*(p2.x-p1.x)/(p2.y-p1.y)+p1.x;
+
+            if (p1.x == p2.x || x <= xinters)
+              counter++;
+          }
+        }
+      }
+    }
+    p1 = p2;
+  }
+
+  if (counter % 2 == 0)
+    return(0);
+  else
+    return(1);
+
+
+
+}
+
+ofxMyVec2f::~ofxMyVec2f()
+{
+    //dtor
+}
---
+++ ofxMyVec2f/src/ofxMyVec2f.h
@@ -0,0 +1,18 @@
+#ifndef OFXMYVEC2F_H
+#define OFXMYVEC2F_H
+#include "ofMain.h"
+#include "ofxVectorMath.h"
+
+class ofxMyVec2f : public ofxVec2f
+{
+    public:
+        ofxMyVec2f();
+
+        int pointPolyTest(vector <ofPoint>* polygon);
+        int pointPolyTest(vector <ofxVec2f>* polygon);
+        virtual ~ofxMyVec2f();
+    protected:
+    private:
+};
+
+#endif // OFXMYVEC2F_H
